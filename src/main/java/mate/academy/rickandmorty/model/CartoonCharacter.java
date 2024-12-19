2e5ff294b44345920ba4b02f37c68975d0072c6a
Mateusz Seler <mateusz.seler@gmail.com>
2024-12-19 14:24:04 +0100

    SECOND STEP: Models implemented, including Entity relations

---
+++ src/main/java/mate/academy/rickandmorty/model/CartoonCharacter.java
@@ -0,0 +1,44 @@
+package mate.academy.rickandmorty.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Data;
+
+@Data
+@Entity
+public class CartoonCharacter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private Long externalId;
+    @Column(unique = true, nullable = false)
+    private String name;
+    private String status;
+    private String species;
+    private String type;
+    private String gender;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "origin_id")
+    private Location origin;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id")
+    private Location location;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "charackter_episodes",
+            joinColumns = @JoinColumn(name = "charachter_id"),
+            inverseJoinColumns = @JoinColumn(name = "episodes_id"))
+    private List<Episode> episodes;
+    private String url;
+    private LocalDateTime created;
+}
---
+++ src/main/java/mate/academy/rickandmorty/model/Location.java
@@ -0,0 +1,23 @@
+package mate.academy.rickandmorty.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private Long externalId;
+    @Column(unique = true, nullable = false)
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String url;
+
+}
